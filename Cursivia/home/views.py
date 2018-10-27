03571b5892d48996fe6cf5dd132a8ffa1b5269f4
Ignacio <iganciorey@hotmail.com>
2018-10-27 19:29:58 -0300

    cambios en registracion y agrego perfil de usuario

--- Cursivia/home/views.py
+++ Cursivia/home/views.py
@@ -163,7 +163,6 @@ class PublicacionCreate(LoginRequiredMixin, CreateView):
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):  
-        print ('materia: ',self.materia)
         titulo = request.POST['titulo']
         cuerpo = request.POST['cuerpo']
         usuario = request.user
@@ -644,9 +643,8 @@ def grupoCreate(request):
         try:
             create_usergroup(request.user, group_name)
             msg = ('Se ha creado el grupo "{0}".').format(group_name)
-            messages.success(request, msg)
-            #return redirect('groups_show', group_name)
-            return redirect('/')
+            messages.success(request, msg) 
+            return redirect('groups_list')
         except GroupError as e:
             error = e.message
 
@@ -675,7 +673,6 @@ def gruposList(request):
 @csrf_exempt
 def foroGrupo(request,group_name):
     group= get_object_or_404(Group, name = group_name)  
-    print('Grupo:',group.id) 
     if request.method=='POST':
 
         cuerpo = request.POST['cuerpo']
