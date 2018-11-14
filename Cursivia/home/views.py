2671f6919756ce2843acb48b359ede82174614d7
Ignacio <iganciorey@hotmail.com>
2018-11-14 13:43:46 -0300

    mod vistas api

--- Cursivia/home/views.py
+++ Cursivia/home/views.py
@@ -28,6 +28,8 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 import datetime
